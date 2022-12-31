a40b94a57bebf01e94742c4897d74ab48a5d3b16
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2022-12-31 14:29:48 +0700

    Hidden navbar in mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import borutoajaLogo from "../assets/Boruto.svg";
 
 export default function Navbar() {
   return (
-    <header className="fixed text-pink-700 shadow-lg shadow-yellow-300/50 bg-gray-800 z-40">
+    <header className="hidden sm:fixed text-pink-700 shadow-lg shadow-yellow-300/50 bg-gray-800 z-40">
       <nav className="flex px-8 py-4 justify-around w-screen items-center ">
         <div className="w-full md:w-1/2 flex items-center">
           <img
